bfdda420402ce6e1957b451a30a4a896b9061af6
dumplant <2437789306@qq.com>
2024-06-06 12:22:47 +0800

    chore: page api

--- app/api/pages/[pageId]/route.ts
+++ app/api/pages/[pageId]/route.ts
@@ -19,11 +19,11 @@ export async function DELETE(
     }
 
     if (!projectId) {
-      return new NextResponse("Server ID missing", { status: 400 });
+      return new NextResponse("Project ID missing", { status: 400 });
     }
 
     if (!params.pageId) {
-      return new NextResponse("Channel ID missing", { status: 400 });
+      return new NextResponse("Page ID missing", { status: 400 });
     }
 
     const project = await db.project.update({
@@ -33,7 +33,7 @@ export async function DELETE(
           some: {
             profileId: profile.id,
             role: {
-              in: [MemberRole.ADMIN, MemberRole.MODERATOR],
+              in: [MemberRole.ADMIN, MemberRole.DEVELOPER],
             },
           },
         },
@@ -54,66 +54,127 @@ export async function DELETE(
   }
 }
 
+// export async function PATCH(
+//   req: Request,
+//   { params }: { params: { pageId: string } }
+// ) {
+//   try {
+//     const profile = await currentProfile();
+//     const { name, type } = await req.json();
+//     const { searchParams } = new URL(req.url);
+
+//     const projectId = searchParams.get("projectId");
+
+//     if (!profile) {
+//       return new NextResponse("Unauthorized", { status: 401 });
+//     }
+
+//     if (!projectId) {
+//       return new NextResponse("Server ID missing", { status: 400 });
+//     }
+
+//     if (!params.pageId) {
+//       return new NextResponse("Channel ID missing", { status: 400 });
+//     }
+
+//     if (name === "general") {
+//       return new NextResponse("Name cannot be 'general'", { status: 400 });
+//     }
+
+//     const project = await db.project.update({
+//       where: {
+//         id: projectId,
+//         members: {
+//           some: {
+//             profileId: profile.id,
+//             role: {
+//               in: [MemberRole.ADMIN, MemberRole.MODERATOR],
+//             },
+//           },
+//         },
+//       },
+//       data: {
+//         pages: {
+//           update: {
+//             where: {
+//               id: params.pageId,
+//               NOT: {
+//                 name: "general",
+//               },
+//             },
+//             data: {
+//               name,
+//               type,
+//             },
+//           },
+//         },
+//       },
+//     });
+
+//     return NextResponse.json(project);
+//   } catch (error) {
+//     console.log("[CHANNEL_ID_PATCH]", error);
+//     return new NextResponse("Internal Error", { status: 500 });
+//   }
+// }
+
+export async function GET(
+  req: Request,
+  { params }: { params: { pageId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    const { searchParams } = new URL(req.url);
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!params.pageId) {
+      return new NextResponse("Page ID missing", { status: 400 });
+    }
+
+    const page = await db.page.findFirst({
+      where: {
+        id: params.pageId,
+      },
+    });
+
+    return NextResponse.json(page);
+  } catch (error) {
+    console.log("[CHANNEL_ID_GET]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
 export async function PATCH(
   req: Request,
   { params }: { params: { pageId: string } }
 ) {
   try {
     const profile = await currentProfile();
-    const { name, type } = await req.json();
-    const { searchParams } = new URL(req.url);
-
-    const projectId = searchParams.get("projectId");
-
+    const { code } = await req.json();
+    console.log("code", code, "params", params);
     if (!profile) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
-    if (!projectId) {
-      return new NextResponse("Server ID missing", { status: 400 });
-    }
-
     if (!params.pageId) {
-      return new NextResponse("Channel ID missing", { status: 400 });
+      return new NextResponse("Page ID missing", { status: 400 });
     }
 
-    if (name === "general") {
-      return new NextResponse("Name cannot be 'general'", { status: 400 });
-    }
-
-    const project = await db.project.update({
+    const page = await db.page.update({
       where: {
-        id: projectId,
-        members: {
-          some: {
-            profileId: profile.id,
-            role: {
-              in: [MemberRole.ADMIN, MemberRole.MODERATOR],
-            },
-          },
-        },
+        id: params.pageId,
       },
       data: {
-        pages: {
-          update: {
-            where: {
-              id: params.pageId,
-              NOT: {
-                name: "general",
-              },
-            },
-            data: {
-              name,
-              type,
-            },
-          },
-        },
+        code,
       },
     });
 
-    return NextResponse.json(project);
+    return NextResponse.json(page);
   } catch (error) {
-    console.log("[CHANNEL_ID_PATCH]", error);
+    console.log("[CHANNEL_ID_CODE_PATCH]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
